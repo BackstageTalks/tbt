deb494028d65d5cad5a27dadfd3c980d65abe75a
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-09-03 15:33:36 +0200

    Delete web/follow-the-data directory

--- web/follow-the-data/config.js
+++
@@ -1,9 +0,0 @@
-window.BLINQ_CONFIG = {
-  basePath: "/follow-the-data/",
-  apiBase: "",
-  authMode: "preview",
-  previewAdmin: true,
-  predictionsDays: 3,
-  refreshMinutes: 5,
-  uiConfigPath: "./ui-config.json"
-};
