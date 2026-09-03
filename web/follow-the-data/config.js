71654ead6bb6b80f9e431c18ea4afe66eba50ce9
BackstageTalks <tennisbackstagetalks@gmail.com>
2026-09-03 15:31:30 +0200

    Add files via upload

---
+++ web/follow-the-data/config.js
@@ -0,0 +1,9 @@
+window.BLINQ_CONFIG = {
+  basePath: "/follow-the-data/",
+  apiBase: "",
+  authMode: "preview",
+  previewAdmin: true,
+  predictionsDays: 3,
+  refreshMinutes: 5,
+  uiConfigPath: "./ui-config.json"
+};
